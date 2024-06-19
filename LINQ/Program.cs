510129113c31a716e1e816540ef2e4128132f503
Douglas <douglas-brumley@hotmail.com>
2024-06-19 15:10:55 -0400

    Excersie 1 Completed

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace LINQ
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            List<Games> NintendoGames = VideoGames();
+            var gametitle = NintendoGames.OrderBy(x => x.NintendoGames.Length).ToList();
+            gametitle.ForEach(x => Console.WriteLine(x.NintendoGames));
+
+            
+
+
         }
+
+            public static List<Games> VideoGames()
+            { 
+            
+            List<Games> output = new List<Games>();
+                output.Add(new Games { NintendoGames = "The Legend of Zelda" });
+                output.Add(new Games { NintendoGames = "Donkey Kong" });
+                output.Add(new Games { NintendoGames = "Metroid" });
+                output.Add(new Games { NintendoGames = "Mario Brothers" });
+                return output;
+
+
+            }
+
+
+        
+
+
+
+
+       
     }
+
+
+
+
+    
 }
